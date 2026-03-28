fd464a4cdbf0a40b4d18436aa1c8f6809becdcbc
doug-lacerda <douglas.lacerda@mensa.org.br>
2026-03-28 14:15:33 -0300

    add color_test.c

---
+++ color_test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main (void)
+{
+	printf("Color test:\n");
+	// 033 é o valor octal para x1b (hexa) e funciona da mesma forma
+	// printf("%sThis is BLACK text\n", "\x1b[30m");
+	// printf("%sThis is RED text\n", "\x1b[31m");
+	// printf("%sThis is GREEN text\n", "\x1b[32m");
+	// printf("%sThis is YELLOW text\n", "\x1b[33m");
+	// printf("%sThis is BLUE text\n", "\x1b[34m");
+	// printf("%sThis is MAGENTA text\n", "\x1b[35m");
+	// printf("%sThis is CYAN text\n", "\x1b[36m");
+	// printf("%sThis is WHITE text\n", "\x1b[37m");
+	// printf("%sThis is NORMAL text\n", "\x1b[m");
+
+	// printf("\n#MUDANDO O BACKGROUND\n");
+	// printf("%sbkg BLACK\n", "\x1b[40m");
+	// printf("%sbkg RED\n", "\x1b[41m");
+	// printf("%sbkg GREEN\n", "\x1b[42m");
+	// printf("%sbkg YELLOW\n", "\x1b[43m");
+	// printf("%sbkg BLUE\n", "\x1b[44m");
+	// printf("%sbkg MAGENTA\n", "\x1b[45m");
+	// printf("%sbkg CYAN\n", "\x1b[46m");
+	// printf("%sbkg WHITE\n", "\x1b[47m");
+	// printf("%sbkg NORMAL\n", "\x1b[m");
+
+}
