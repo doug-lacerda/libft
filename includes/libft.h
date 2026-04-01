4e969fef51b900d112cffae24fefb3a32f1242cc
doug-lacerda <douglas.lacerda@mensa.org.br>
2026-03-31 21:41:57 -0300

    add+ em /includes/libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -1,8 +1,15 @@
+#ifndef LIBFT_H		// 1. "ei, o LIBFT_H já existe?"
+# define LIBFT_H	// 2. "Não? Então agora ele existe!"
 
-#include <unistd.h>
+// Inclusão de outras bibliotecas
+# include <unistd.h>
 
+// 3. Os protótipos das minhas funções (as "promessas")
 /*
 **	string/
 */
 void	ft_putchar(char c);
 void	ft_putendl(char *str);
+
+#endif			// 4. "Fim da proteção. Daqui pra baixo o LIBFT_H não me 
+				//	ignora mais."
