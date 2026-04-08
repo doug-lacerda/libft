5757438ddd779dae60ce899f5db6a559990226e5
doug-lacerda <douglas.lacerda@mensa.org.br>
2026-04-07 21:27:03 -0300

    add /string/ft_strchr.c

---
+++ string/ft_strchr.c
@@ -0,0 +1,11 @@
+char	*ft_strchr(char *str, char to_find)
+{
+	int i;
+
+	i = 0;
+	while (str[i] && str[i] != to_find)
+		i++;
+	if (str[i] == to_find)
+		return (&str[i]);
+	return ("NULL");				//NULL estava sem aspas (nenhuma)
+}
